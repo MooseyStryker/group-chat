bebe97c7340d2010f5d640a414303cb939738d0f
Stryker Huber <stryker.important@gmail.com>
2025-01-29 14:09:33 -0600

    scripts added

---
+++ backend/db/migrations/20250129191909-adding-first-and-last-name.js
@@ -0,0 +1,55 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+
+    const addColumn = async (columnName) => {
+      await queryInterface.addColumn('Users', `${columnName}`, {
+        type: Sequelize.STRING,
+        allowNull: true
+      })
+
+      await queryInterface.sequelize.query(`
+        UPDATE "Users"
+        SET "${columnName}" = 'DefaultValue'
+        WHERE "${columnName}" IS NULL
+      `);
+
+      await queryInterface.changeColumn('Users', `${columnName}`, {
+        type: Sequelize.STRING,
+        allowNull: false
+      })
+
+    }
+
+    await addColumn('firstName')
+    await addColumn('lastName')
+
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.removeColumn('Users', 'firstName', {
+      type: Sequelize.STRING,
+      allowNull: false,
+    })
+
+    await queryInterface.removeColumn('Users', 'lastName', {
+      type: Sequelize.STRING,
+      allowNull: false
+    })
+  },
+
+};
---
+++ backend/utils/validation.js
@@ -0,0 +1,23 @@
+const { validationResult } = require('express-validator')
+
+// Reorders the error handling from express-validator middleware to JSON
+const handleValidationErrors = (req, _res, next) => {
+    const validationError = validationResult(req)
+
+    if (!validationError.isEmpty()){
+        const errors = {}
+        handleValidationErrors.array().forEach(error => errors[error.path] = error.msg)
+
+        const err = Error("Bad request.")
+        err.errors = errors;
+        err.status = 400
+        err.title = "Bad request"
+        next(err)
+    }
+
+    next();
+}
+
+module.exports = {
+    handleValidationErrors
+}
