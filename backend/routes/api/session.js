9d6dfaf30ac7ad73ae605e33c48e48010f5bbae6
Stryker Huber <stryker.important@gmail.com>
2025-01-29 10:45:04 -0600

    logout added

---
+++ backend/routes/api/session.js
@@ -0,0 +1,64 @@
+const express = require('express')
+const { Op } = require('sequelize')
+const bcrypt = require('bcryptjs')
+
+const { setTokenCookie, restoreUser} = require('../../utils/auth')
+const { User } = require('../../db/models')
+
+const router = express.Router()
+
+// User Login
+router.post('/', async (req, res, next) =>{
+    const { credential, password } = req.body
+
+    const user = await User.unscoped().findOne({
+        where:{
+            [Op.or]: {
+                username: credential,
+                email: credential
+            }
+        }
+    })
+
+    // Password is stored in a hash using bcrypt
+    if (!user || !bcrypt.compareSync(password, user.hashedPassword.toString())){
+        const err = new Error('Login Failed')
+        err.status = 401
+        err.title = "Login failed"
+        err.errors = {
+            credential: 'The provided credentials were invalid.'
+        }
+
+        return next(err)
+    }
+
+    const safeUser = {
+        id: user.id,
+        email: user.email,
+        username: user.username
+    }
+
+    await setTokenCookie(res, safeUser)
+
+    return res.json({
+        user: safeUser
+    })
+
+})
+
+
+// Logout middleware
+router.delete('/', (_req, res) => {
+    res.clearCookie('token')
+    return res.json({
+        message: 'Success'
+    })
+})
+
+
+
+
+
+
+
+module.exports = router
---
+++ backend/utils/auth.js
@@ -0,0 +1,86 @@
+// We pull the jwtConfig from config/index.js file which pulls the secret and expiresIn from the .env file
+const jwt = require('jsonwebtoken')
+const { jwtConfig } = require('../config')
+const { User } = require('../db/models')
+
+const { secret, expiresIn } = jwtConfig
+
+
+// Sends out the JWT Cookie for the browser to use
+const setTokenCookie = (res, user) => {
+    // This creates our JWT
+    const safeUser = {
+        id: user.id,
+        email: user.email,
+        username: user.username
+    };
+
+    const token = jwt.sign(
+        {
+            data: safeUser
+        },
+        secret,
+        {
+            expiresIn: parseInt(expiresIn) // We use parseInt due to issues with the data being transfered from the .env file becomes a string rather than an integer
+        }
+    );
+
+    const isProduction = process.env.NODE_ENV === 'production'
+
+    // Sets our token cookie
+    res.cookie('token', token, {
+        maxAge: expiresIn * 1000, // maxAge uses milliseconds, expiresIn is regular seconds
+        httpOnly: true,
+        secure: isProduction,
+        sameSite: isProduction && 'Lax'
+    });
+
+    return token
+}
+
+const restoreUser = (req, res, next) => {
+    // token that's parsed from the cookies in the browser
+    const { token } = req.cookies
+    req.user = null;
+
+    return jwt.verify(token, secret, null, async (err, jwtPayload) => {
+        if (err) {
+            return next()
+        }
+
+        try {
+            const { id } = jwtPayload.data
+            req.user = await User.findByPk(id, {
+                attributes:{
+                    include: ['email', 'createdAt', 'updatedAt']
+                }
+            })
+        } catch (error) {
+            res.clearCookie('token')
+            return next()
+        }
+
+        if (!req.user) res.clearCookie('token');
+
+        return next();
+    })
+}
+
+// To ensures the pages are secured from non-users, this will produce an error in the backend. We'll add more errors in the frontend to guide the user to signup
+const requireAuth = function (req, _res, next){
+    if (req.user) return next();
+
+    const err = new Error('Authentication required')
+    err.title = 'Authentication required'
+    err.errors = {
+        message: 'User not found, authentication required'
+    }
+    err.status = 401
+    return next(err)
+}
+
+module.exports = {
+    setTokenCookie,
+    restoreUser,
+    requireAuth
+}
