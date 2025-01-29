023b9c5f4b92fdbad08a523267f46809df695490
Stryker Huber <stryker.important@gmail.com>
2025-01-29 14:37:37 -0600

    this one again pt 6 bagaloo

---
+++ backend/db/migrations/20250129191909-adding-first-and-last-name.js
@@ -0,0 +1,63 @@
+'use strict';
+
+// Allows to bypass render.com small db restrictions
+let options = {}
+if (process.env.NODE_ENV === 'production'){
+  options.schema = process.env.SCHEMA
+}
+
+options.tableName = "Users"
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+
+    const addColumn = async (columnName) => {
+      await queryInterface.addColumn(options, `${columnName}`, {
+        type: Sequelize.STRING,
+        allowNull: true
+      })
+
+      await queryInterface.sequelize.query(`
+        UPDATE "Users"
+        SET "${columnName}" = 'DefaultValue'
+        WHERE "${columnName}" IS NULL
+      `);
+
+      await queryInterface.changeColumn(options, `${columnName}`, {
+        type: Sequelize.STRING,
+        allowNull: false
+      })
+
+    }
+
+    await addColumn('firstName')
+    await addColumn('lastName')
+
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.removeColumn(options, 'firstName', {
+      type: Sequelize.STRING,
+      allowNull: false,
+    })
+
+    await queryInterface.removeColumn(options, 'lastName', {
+      type: Sequelize.STRING,
+      allowNull: false
+    })
+  },
+
+};
