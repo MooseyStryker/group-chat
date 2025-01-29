3f71ab8928193fdf7b76eef003fbad7eb7f83c20
Stryker Huber <stryker.important@gmail.com>
2025-01-29 12:15:05 -0600

    Added user sign up, tested model validations for unique username and email. Verified the db stores hash pass not actual pass

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,5 +1,35 @@
 const express = require('express')
+const bcrypt = require('bcryptjs')
+
+const { setTokenCookie, requireAuth } = require('../../utils/auth')
+const { User } = require('../../db/models')
+
 const router = express.Router()
+
+// User Signup
+router.post('/', async (req,res) => {
+    const { email, password, username } = req.body
+
+    // Store the hashed password, not the actual password
+    const hashedPassword = bcrypt.hashSync(password)
+    const user = await User.create({
+        email,
+        username,
+        hashedPassword
+    })
+
+    const safeUser = {
+        id: user.id,
+        email: user.email,
+        user: user.username
+    }
+
+    await setTokenCookie(res, safeUser)
+
+    return res.json({
+        user: safeUser
+    })
+})
 
 
 module.exports = router
