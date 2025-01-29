278b3539bdfc04d0a64554ea7f8652edcc407a1c
Stryker Huber <stryker.important@gmail.com>
2025-01-29 09:51:55 -0600

    Ran and tested requireAuth and setting session tokens in the browser. Deleted the tests and left restoreUser to connect to api routers

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router()
+const { restoreUser } = require('../../utils/auth')
 
-router.post('/test', function(req,res) {
-    res.json({ requestBody: req.body})
-})
-
+// This was used to test require auth and setting session tokens.
+// This will now be used to conect restoreUser middleware to the API router
+// If current user session is valid, restoreUser will set req.user to the user in the db
+// Else, it will set req.user to null
+router.use(restoreUser)
 
 
 module.exports = router
