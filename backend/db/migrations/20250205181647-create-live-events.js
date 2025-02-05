c568f5464b21eb78904e72df7414ab964d094f43
Stryker Huber <stryker.important@gmail.com>
2025-02-05 13:16:06 -0600

    added live events

---
+++ backend/db/migrations/20250205181647-create-live-events.js
@@ -0,0 +1,61 @@
+'use strict';
+
+// Allows to bypass render.com small db restrictions
+let options = {}
+if (process.env.NODE_ENV === 'production'){
+  options.schema = process.env.SCHEMA
+}
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('LiveEvents', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        type: Sequelize.INTEGER
+      },
+      groupId: {
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      repeat: {
+        type: Sequelize.ENUM('daily', 'weekly', 'monthly'),
+        allowNull: false
+      },
+      private: {
+        type: Sequelize.BOOLEAN,
+        allowNull:false
+      },
+      privateInvitation: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+          len: [16, 16]
+        }
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+
+    options.tableName = 'LiveEvents'
+    await queryInterface.dropTable(options);
+  }
+};
---
+++ backend/db/models/liveevents.js
@@ -0,0 +1,58 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class LiveEvents extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  LiveEvents.init({
+    userId: {
+      type:DataTypes.INTEGER
+    },
+    groupId: {
+      type:DataTypes.INTEGER
+    },
+    name: {
+      type:DataTypes.STRING,
+      allowNull:false,
+      validate: {
+        len: [3,256]
+      }
+    },
+    description: {
+      type:DataTypes.STRING,
+      allowNull:false,
+      validate:{
+        len: [5, 3000]
+      }
+    },
+    repeat: {
+      type:DataTypes.ENUM,
+      allowNull: false,
+      values: ['daily', 'weekly', 'monthly']
+    },
+    private: {
+      type:DataTypes.BOOLEAN,
+      allowNull: false
+    },
+    privateInvitation:{
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [16,16]
+      }
+    }
+  }, {
+    sequelize,
+    modelName: 'LiveEvents',
+  });
+  return LiveEvents;
+};
