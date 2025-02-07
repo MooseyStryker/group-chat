3d2fc1913c1d073c2761eb4ccd20774da1cdea0a
YacineBenzo <yacine.benziane@gmail.com>
2025-02-07 18:11:35 +0100

    Installed Nextjs and some example folders

---
+++ frontend/src/app/new-page/nested-folder/page.js
@@ -0,0 +1,18 @@
+
+"use client"
+
+export default function newPage(){
+
+    console.log('Is this server of nested folder')
+
+
+    return(
+
+
+        <div className="main-layout">
+
+            <h1>Nested page</h1>
+
+        </div>
+    )
+}
