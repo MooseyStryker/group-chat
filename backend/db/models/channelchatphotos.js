9adbcc5f5d79374f9509d0fd4928e32ee94e845a
KlausMass <massinemouha@zohomail.com>
2025-02-08 01:15:42 +0100

    Added typing for dataValues helping ease the remembering values game

--- backend/db/models/channelchatphotos.js
+++ backend/db/models/channelchatphotos.js
@@ -1,27 +1,38 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
+
+/**
+ * @typedef {object} ChannelChatPhotoAttributes
+ * @property {number} channelChatId - The ID of the channel chat.
+ * @property {number} userId - The ID of the user who uploaded the photo.
+ * @property {string} [imgAWSLink] - The AWS link for the photo (optional).
+ */
+
+/**
+ * @typedef {ChannelChatPhotoAttributes & { id: number, createdAt: Date, updatedAt: Date }} ChannelChatPhotoInstance
+ */
+
+/**
+ * @param {import('sequelize').Sequelize} sequelize - The Sequelize instance.
+ * @param {typeof DataTypes} DataTypes - The Sequelize DataTypes.
+ * @returns {typeof Model<ChannelChatPhotoAttributes>}
+ */
 module.exports = (sequelize, DataTypes) => {
   class ChannelChatPhoto extends Model {
     /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
+     * @param {object} models - All defined models
+     * @returns {void}
      */
     static associate(models) {
-      // define association here
-
-      ChannelChatPhoto.belongsTo(models.ChannelChat,{
+      ChannelChatPhoto.belongsTo(models.ChannelChat, {
         foreignKey: 'channelChatId'
-      })
+      });
 
-      ChannelChatPhoto.belongsTo(models.User,{
-        foreignKey: 'channelChatId'
-      })
+      ChannelChatPhoto.belongsTo(models.User, {
+        foreignKey: 'userId' // Correct foreign key here
+      });
     }
   }
-
 
   ChannelChatPhoto.init({
     channelChatId: {
@@ -32,7 +43,7 @@ module.exports = (sequelize, DataTypes) => {
       }
     },
     userId: {
-      type:DataTypes.INTEGER,
+      type: DataTypes.INTEGER,
       references: {
         model: 'User',
         key: 'id'
@@ -45,5 +56,6 @@ module.exports = (sequelize, DataTypes) => {
     sequelize,
     modelName: 'ChannelChatPhoto',
   });
+
   return ChannelChatPhoto;
 };
--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -1,123 +1,88 @@
 'use strict';
 
-const {  Model, Validator } = require('sequelize');
+const { Model, Validator } = require('sequelize');
 
+/**
+ * @typedef {object} UserAttributes
+ * @property {string} firstName - The user's first name.
+ * @property {string} lastName - The user's last name.
+ * @property {string} username - The user's username.
+ * @property {string} email - The user's email address.
+ * @property {string} hashedPassword - The user's hashed password.
+ */
+
+/**
+ * @typedef {UserAttributes & { id: number, createdAt: Date, updatedAt: Date }} UserInstance
+ */
+
+
+/**
+ * @param {import('sequelize').Sequelize} sequelize - The Sequelize instance.
+ * @param {typeof DataTypes} DataTypes - The Sequelize DataTypes.
+ * @returns {typeof Model<UserAttributes>}
+ */
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
+     * @param {object} models - All defined models
+     * @returns {void}
      */
     static associate(models) {
-      // define association here
-
-      User.hasMany(models.Group, {
-        foreignKey: 'organizerId',
-        onDelete: 'CASCADE',
-        hooks: true
-      })
-
-      User.hasMany(models.Channel, {
-        foreignKey: 'channelCreatorId',
-        onDelete: 'CASCADE',
-        hooks: true
-      })
-
-      User.hasMany(models.ChannelChat, {
-        foreignKey: 'userId',
-        onDelete: 'CASCADE',
-        hooks: true
-      })
-
-      User.hasMany(models.ChannelChatPhoto, {
-        foreignKey: 'userId',
-        onDelete: 'CASCADE',
-        hooks: true
-      })
-
-      User.hasMany(models.LiveEvent, {
-        foreignKey: 'userId',
-        onDelete: 'CASCADE',
-        hooks: true
-      })
-
-      User.hasMany(models.GroupMembership, {
-        foreignKey: 'memberId',
-        onDelete: 'CASCADE',
-        hooks: true
-      })
-
-      User.hasMany(models.ChannelChatReply, {
-        foreignKey: 'userId',
-        onDelete: 'CASCADE',
-        hooks: true
-      })
-
-      User.hasMany(models.AttendanceLiveEvent, {
-        foreignKey: 'userId',
-        onDelete: 'CASCADE',
-        hooks: true
-      })
-
+      User.hasMany(models.Group, { foreignKey: 'organizerId', onDelete: 'CASCADE', hooks: true });
+      User.hasMany(models.Channel, { foreignKey: 'channelCreatorId', onDelete: 'CASCADE', hooks: true });
+      User.hasMany(models.ChannelChat, { foreignKey: 'userId', onDelete: 'CASCADE', hooks: true });
+      User.hasMany(models.ChannelChatPhoto, { foreignKey: 'userId', onDelete: 'CASCADE', hooks: true });
+      User.hasMany(models.LiveEvent, { foreignKey: 'userId', onDelete: 'CASCADE', hooks: true });
+      User.hasMany(models.GroupMembership, { foreignKey: 'memberId', onDelete: 'CASCADE', hooks: true });
+      User.hasMany(models.ChannelChatReply, { foreignKey: 'userId', onDelete: 'CASCADE', hooks: true });
+      User.hasMany(models.AttendanceLiveEvent, { foreignKey: 'userId', onDelete: 'CASCADE', hooks: true });
     }
   }
-  User.init({
-    firstName:{
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [3, 40]
-      }
-    },
-    lastName:{
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [3, 40]
-      }
-    },
-    username: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-      validate:{
-        len: [4, 30],
-        isNotEmail(value){
-          if (Validator.isEmail(value)){
-            throw new Error("An email in the username is not allowed")
+
+  User.init(
+    {
+      firstName: { type: DataTypes.STRING, allowNull: false, validate: { len: [3, 40] } },
+      lastName: { type: DataTypes.STRING, allowNull: false, validate: { len: [3, 40] } },
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          len: [4, 30],
+          isNotEmail(value) {
+            if (Validator.isEmail(value)) {
+              throw new Error("An email in the username is not allowed");
+            }
           }
+        }
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          len: [3, 256],
+          isEmail: true
+        }
+      },
+      hashedPassword: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [60, 60]
         }
       }
     },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-      validate: {
-        len: [3, 256],
-        isEmail: true
-      }
-    },
-    hashedPassword: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [60,60]
+    {
+      sequelize,
+      modelName: 'User',
+      defaultScope: {
+        attributes: {
+          exclude: ['hashedPassword', 'email', 'createdAt', 'updatedAt']
+        }
       }
     }
-  }, {
-    sequelize,
-    modelName: 'User',
+  );
 
-    // The reason we removed the elements from the scope, is to prevent this data being leaked through the React frontend
-    defaultScope:{
-      attributes:{
-        exclude:[
-          'hashedPassword', 'email', 'createdAt', 'updatedAt'
-        ]
-      }
-    }
-  });
   return User;
 };
