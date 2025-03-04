709073782aaba4c85bf396819434a3f618aeda62
Stryker Huber <stryker.important@gmail.com>
2025-03-04 12:48:58 -0600

    refactored csrf to for next.js instead of vite. Allows frontend to be proxy

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,37 @@
 const express = require('express');
 const router = express.Router();
 
+const apiRouter = require('./api')
+
+router.use('/api', apiRouter)
+
+
+// This is our static routes
+// Serves our React build files in production
+if (process.env.NODE_ENV === 'production') {
+  const path = require('path');
+  
+  // Serve the frontend's index.html file at the root route
+  router.get('/', (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    res.sendFile(
+      path.resolve(__dirname, '../../frontend', 'dist', 'index.html')
+    );
+  });
+
+  // Serve the static assets in the frontend's build folder
+  router.use(express.static(path.resolve("../frontend/dist")));
+
+  // Serve the frontend's index.html file at all other routes NOT starting with /api
+  router.get(/^(?!\/?api).*/, (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    res.sendFile(
+      path.resolve(__dirname, '../../frontend', 'dist', 'index.html')
+    );
+  });
+}
+
+
 /**
  * @swagger
  * /api/csrf/restore:
@@ -20,28 +51,15 @@ const router = express.Router();
  *                   example: "a1b2c3d4e5f6g7h8i9j0"
  *                   description: The CSRF token
  */
-router.get("/api/csrf/restore", (req, res) => {
+// Add a XSRF-TOKEN cookie in development
+if (process.env.NODE_ENV !== 'production') {
+  router.get("/api/csrf/restore", (req, res) => {
     const csrfToken = req.csrfToken();
     res.cookie("XSRF-TOKEN", csrfToken);
     res.status(200).json({
-        'XSRF-Token': csrfToken
-    })
-})
-
-// Add a XSRF-TOKEN cookie in development
-if (process.env.NODE_ENV !== 'production') {
-    router.get("/api/csrf/restore", (req, res) => {
-      const csrfToken = req.csrfToken();
-      res.cookie("XSRF-TOKEN", csrfToken);
-      res.status(200).json({
-        'XSRF-Token': csrfToken
-      });
+      'XSRF-Token': csrfToken
     });
+  });
 }
-
-
-const apiRouter = require('./api')
-
-router.use('/api', apiRouter)
 
 module.exports = router
